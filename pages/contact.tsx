3765fa785308fdfa24283d53dbf8fc9ed5b3de1b
Chekkal Youcef <81917154+Dev-Dz27@users.noreply.github.com>
2022-10-10 13:46:46 +0100

    Add files via upload

---
+++ pages/contact.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import About from '../components/About'
+import Contact from '../components/Contact'
+import Footer from "../components/Footer";
+
+
+const contact = () => {
+  return (
+    <div >
+        <About/>
+        <Contact/>
+
+    </div>
+  )
+}
+
+export default contact
